f9502258ebdaaf77d78dd5d28607c5da5e46f28b
khushthecoder <chaudharikhush0711@gmail.com>
2025-09-22 23:46:08 +0530

    Revert "cf questions"
    This reverts commit 4ca248d5e503e7ba24a50fe5f48002046dc5b94a.

---
+++ syntax/lec10(half-pyramid).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+// Newton School Online C++ compiler to run C++ online.
+// Write C++ code in this online editor and run it.
+
+int main() {
+    int n;
+    cin>>n;
+    for(int row=0;row<n;row++){
+        for(int col=0;col<row+1;col++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    // print a inverted pyramid
+    for (int r=0;r<n;r++){
+        for(int c=0;c<n-r;c++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
---
+++ syntax/lec5(loop).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Newton School Online C++ compiler to run C++ online.
+// Write C++ code in this online editor and run it.
+
+int main() {
+    for(int i=0;i<10;i=i+2){
+        cout<<i<<endl;
+    }
+    for(int i=0;i<5;i++){
+        cout<<"a"<<" ";
+        cout<<endl;
+    }
+}
